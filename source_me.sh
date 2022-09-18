cbe211be84a6edccf3a7695b1ef27138dae0d6c8
HFDan <58553089+HFDan@users.noreply.github.com>
2022-09-18 16:42:18 +0300

    Changed source_me

--- source_me.sh
+++ source_me.sh
@@ -1,7 +1,28 @@
 export DATE=$(date '+%Y-%b-%d')
 
-function compile() {
-  clang++ -masm=intel -g -O0 -Wall -Wpedantic --std=c++20 $@
+if [[ -f "/bin/clang++" ]]; then
+  CXX="/bin/clang++"
+  CC="/bin/clang"
+elif [[ -f "/bin/g++" ]]; then
+  CXX="/bin/g++"
+  CC="/bin/gcc"
+else
+  CXX="NONE"
+  CC="NONE"
+fi
+
+function compilepp() {
+  "$CXX" -masm=intel -g -O0 -Wall -Wpedantic --std=c++20 $@
 }
 
-echo "Use \"compile\" to compile a c++ program"
+function compilec() {
+  "$CC" -masm=intel -g -O0 -Wall -Wpedantic --std=c11 $@
+}
+
+if [[ "$CC" != "NONE" ]]; then
+  echo -e "C++ compiler autodetected as $(${CXX} --version | head -n 1)\nC compiler autodetected as $(${CC} --version | head -n 1)"
+  echo -e "Use \"compilepp\" to compile a C++ program and \"compilec\" to compile a C program"
+else
+  echo -e "No valid C/C++ compilers found"
+fi
+
