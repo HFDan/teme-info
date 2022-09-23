44273f1437dc5a71d8b01407ccde33a297ef4778
HFDan <58553089+HFDan@users.noreply.github.com>
2022-09-23 20:42:39 +0300

    Tema de azi

---
+++ 2022-Sep-23/main-1.cpp
@@ -0,0 +1,97 @@
+/*
+Prin înjumătățirea unui număr natural se înțelege înlocuirea fiecărei cifre
+pare cu jumătatea ei. De exemplu,
+prin înjumătățirea numărului 5622 se obține numărul 5311.
+Fișierul atestat.in conține pe prima linie un număr natural n (2≤n≤100), iar pe a doua linie, un șir
+de n numere naturale cu cel mult 9 cifre fiecare.
+Se consideră subprogramele:
+
+verif care are un singur parametru x (număr natural cu maxim 9 cifre) și returnează valoarea 1
+dacă toate cifrele numărului x sunt pare sau valoarea 0, în caz contrar.
+
+modif care are ca unic parametru numărul natural x. Subprogramul înjumătățește valoarea lui x
+(conform definiției de mai sus) și furnizează numărul modificat prin intermediul aceluiași parametru.
+
+Cerințe:
+a. Să se scrie definiția completă a subprogramului verif;
+b. Să se scrie definiția completă a subprogramului modif;
+c. Să se scrie un program care citește din fișierul atestat.in numărul n și cele n elemente ale
+tabloului unidimensional v și, folosind apeluri utile ale subprogramelor verif și modif, determină
+înjumătățirea (conform definiției de mai sus) a elementelor tabloului care au toate cifrele pare.
+Programul scrie pe prima linie a fișierului atestat.out elementele tabloului modificat.
+Elementele tabloului care conțin cel puțin o cifră impară nu se modifică
+*/
+
+// I hate this include
+#ifndef _WIN32
+#include <bits/stdc++.h>
+#else
+#include "./stdc++.h"
+#endif
+
+bool verif(int num) {
+    while (num != 0) {
+
+        if ((num % 10) % 2 != 0)
+            return false;
+
+        num /= 10;
+    }
+    
+    return true;
+}
+
+// Don't know why, but i changed random stuff and now it works
+// Doing this shit is considered "bad coding practice"
+// But if you do it fast enough, it's "machine learning"
+// And it pays 10x your current salary
+void modif(int& x) {
+    int numaruPeDos = 0;
+
+    while (x != 0) {
+        uint8_t cifra = x % 10;
+
+        if (cifra % 2 == 0)
+            cifra /= 2;
+
+        numaruPeDos += cifra;
+        numaruPeDos *= 10;
+
+        x /= 10;
+    }
+
+    numaruPeDos /= 10;
+
+    {
+        int ogl = 0;
+        int cpcp = numaruPeDos;
+        while (cpcp != 0) {
+            ogl = ogl * 10 + cpcp % 10;
+            cpcp /= 10;
+        }
+
+        numaruPeDos = ogl;
+    }
+
+    x = numaruPeDos;
+}
+
+int main(int argc, char** argv) {
+
+    std::ifstream file("atestat.in");
+    std::ofstream out("atestat.out");
+    long sz;
+    file >> sz;
+
+    for (long i = 0; i < sz; i++) {
+        int nr;
+        file >> nr;
+
+        if (verif(nr))
+            modif(nr);
+        
+        out << nr << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ 2022-Sep-23/main-2.cpp
@@ -0,0 +1,110 @@
+/*
+Fișierul atestat.in conține pe prima linie un număr natural n, 2≤n≤100 și pe a doua linie n numere
+naturale cu cel puțin 2 și cel mult 6 cifre, separate printr-un spațiu.
+Se consideră subprogramele:
+
+
+inversareCifre cu un parametru x, prin intermediul căruia primește un număr natural format
+din cel mult 6 cifre. Subprogramul modifică valoarea lui x, inversând ordinea cifrelor lui, cu excepția
+primei cifre care rămâne în aceeași poziție. De exemplu, pentru valoarea 21754 a parametrului x,
+în urma executării subprogramului, valoarea furnizată prin parametrul x va fi 24571.
+
+
+nrDivizori cu un parametru x, prin intermediul căruia primește un număr natural nenul, format
+din cel mult 6 cifre. Subprogramul returnează numărul divizorilor parametrului x.
+
+
+Cerințe:
+a. Să se scrie definiția completă a subprogramului inversareCifre;
+b. Să se scrie definiția completă a subprogramului nrDivizori;
+c. Să se scrie un program care citește din fișierul atestat.in numărul n și cele n numere naturale,
+iar apoi, folosind apeluri utile ale subprogramelor inversareCifre și nrDivizori, modifică
+fiecare număr din șir care are mai mult de 4 divizori, inversând ordinea tuturor cifrelor lui, cu
+excepția primei cifre care rămâne în aceeași poziție și scrie în fișierul atestat.out, pe prima
+linie, toate numerele din șirul modificat. Dacă nu există numere cu mai mult de 4 divizori se va
+scrie în fișier, mesajul "nu au fost facute modificari"
+*/
+
+#ifndef _WIN32
+#include <bits/stdc++.h>
+#else
+#include "./stdc++.h"
+#endif
+
+// I am so tempted to use strings, so heres a toggle
+void inversareCifre(int& x, bool useStrings = false) {
+    if (useStrings) {
+        std::string str = std::to_string(x);
+        std::reverse(str.begin() + 1, str.end());
+        x = std::stoi(str);
+    } else {
+        // We can do it the *math* way (yuck, cringe, 🤮)
+        // Or we can do it the *fundular entertainment* way (Based, max0r reference) 
+
+        int digits[6] = {0}; // Yes, this works.
+        int iteratorBazat; // We will need this later
+        for (iteratorBazat = 0; x > 0; iteratorBazat++) {
+            digits[iteratorBazat] = x % 10;
+            x /= 10;
+        }
+
+        // We reverse the digits...
+        for (int i = 0, j = iteratorBazat - 1; i <= j; j--, i++)
+            std::swap(digits[i], digits[j]);
+
+        // We reverse what we need...
+        for (int i = 1, j = iteratorBazat - 1; i <= j; j--, i++)
+            std::swap(digits[i], digits[j]);
+        
+        // Rebuild the number
+        x = 0;
+        for (int i = 0; i < iteratorBazat; i++) {
+            x = x * 10 + digits[i];
+        }
+    }
+}
+
+
+inline constexpr int nrDivizori(const int& x) {
+    int div = 0;
+
+    for (int i = 1; i <= x; i++)
+        if (x % i == 0)
+            div++;
+
+    return div;
+}
+
+static int TimesFunctionWasCalled = 0;
+int main(int argc, char** argv) {
+    freopen("atestat.in", "r", stdin);
+    freopen("atestat.out", "w", stdout);
+
+    int n;
+    std::cin >> n;
+
+    int* const arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        std::cin >> num;
+
+        if (nrDivizori(num) > 4) {
+            inversareCifre(num);
+            TimesFunctionWasCalled++;
+        }
+        arr[i] = num;
+    }
+
+    if (!TimesFunctionWasCalled) {
+        std::cout << "Nu au fost facute modificari" << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << std::endl;
+    }
+
+    delete[] arr;
+    return EXIT_SUCCESS;
+}
