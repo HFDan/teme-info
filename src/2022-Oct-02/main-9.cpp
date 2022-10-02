3d5fe707a3dc38beb06572704d631f77932f189e
Dani <58553089+HFDan@users.noreply.github.com>
2022-10-02 22:39:13 +0300

    Improved formatting

--- src/2022-Oct-02/main-9.cpp
+++ src/2022-Oct-02/main-9.cpp
@@ -23,28 +23,28 @@ kys_t findPos() {
         int max = 0;
         for(int i = 1; i <= n / 2; i++)
             for(int j = i + 1; j < n + 1 - i; j++)
-                if(mat[i][j]>max) {max=mat[i][j]; imax=i; jmax=j; }
+                if( mat[i][j] > max ) { max=mat[i][j]; imax=i; jmax=j; }
     };
 
     auto maxS = [&](int& imax, int& jmax) -> void {
         int max = 0;
         for(int i = n / 2 + 1; i <= n; i++)
             for(int j= n + 1 - i + 1; j < i; j++)
-                if(mat[i][j]>max) {max=mat[i][j]; imax=i; jmax=j; }
+                if( mat[i][j] > max ) { max=mat[i][j]; imax=i; jmax=j; }
     };
 
     auto maxV = [&](int& imax, int& jmax) -> void {
         int max = 0;
         for(int i = 2; i <= n - 1; i++)
             for(int j = 1; j < std::min(i,n+1-i); j++)
-                if(mat[i][j]>max) {max=mat[i][j]; imax=i; jmax=j; }
+                if( mat[i][j] > max ) { max=mat[i][j]; imax=i; jmax=j; }
     };
 
     auto maxE = [&](int& imax, int& jmax) -> void {
         int max = 0;
         for(int i = 2; i <= n - 1; i++)
             for(int j = std::max(i,n+1-i) + 1; j <= n; j++)
-                if(mat[i][j]>max) {max=mat[i][j]; imax=i; jmax=j; }
+                if( mat[i][j] > max ) { max=mat[i][j]; imax=i; jmax=j; }
     };
 
     std::pair<int, int> N, S, V, E;
